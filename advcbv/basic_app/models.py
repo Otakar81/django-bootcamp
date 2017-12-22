2681ef301dcf8cd63079ff9fdb7b8dc4ee7ebad7
Otakar81Cube <34288632+Otakar81@users.noreply.github.com>
2017-12-22 15:04:46 +0100

    Fine progetto

--- advcbv/basic_app/models.py
+++ advcbv/basic_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+
+# Django crea "gratis" un campo numerico id, che usa come PK
 class School(models.Model):
     name = models.CharField(max_length=256)
     principal = models.CharField(max_length=256)
@@ -9,11 +12,19 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
+    # Necessario per la CreateView. Deve sapere dove andare di default dopo l'inserimento
+    def get_absolute_url(self):
+        return reverse("basic_app:detail", kwargs={'pk': self.pk})
+
+
 
 
 class Student(models.Model):
     name = models.CharField(max_length=256)
     age = models.PositiveIntegerField()
+
+    # Il "related name" è il nome dell'attributo con cui far riferimento a questa FK dalla classe School
+    # L'elenco degli studenti della scuola, sarà ottenibile quindi come campo "students"
     school = models.ForeignKey(School, related_name='students')
 
     def __str__(self):
--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse_lazy
 
 from . import models
 
@@ -32,9 +33,51 @@ class IndexView(TemplateView):
 
 # CBV che implementa la GenericView pensata per stampare una lista di elementi di un modello
 class SchoolListView(ListView):
+
+    # La classe ListView crea "gratis" un context di ritorno il cui nome è il nome della classe del modello
+    # seguita da _list, tutto minuscolo
+    # In questo caso, senza far nulla, la lista di oggetti sarà restituita alla view col nome "school_list"
+    # Possiamo cambiare il nome di questo context facendo override della proprietà che segue
+    context_object_name = 'schools' # Nome "personalizzato" del context restituito nella view
+
     model = models.School
 
 # CBV che implementa la stampa del dettaglio di un elemento del modello
 class SchoolDetailView(DetailView):
+
+    # Il nome di default del context restituito dalla DetailView è il nome del modello in lowecase
+    # In questo caso avremmo "school"
+    # Personalizziamo
+    context_object_name = 'school_detail'
+
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+# CRUD: Create
+class SchoolCreateView(CreateView):
+
+    # Di default, la CreateView si aspetta l'esistenza di un template di nome MODELNAME_form.html
+    # In questo caso: school_form.html
+
+    # In una CreateView DEVO specificare quali campi possono essere inseriti
+    fields = ('name', 'principal', 'location')
+
+    model = models.School
+
+
+# Update: le caratteristiche sono molto simili a quelle della create
+class SchoolUpdateView(UpdateView):
+
+    # Ipotizziamo che nome e principal si possano modigicare, mentre la location di una scuola difficilmente cambia
+    fields = ('name', 'principal')
+
+    model = models.School
+
+# Delete
+class SchoolDeleteView(DeleteView):
+
+    # Il nome di default del context restituito dalla DetailView è il nome del modello in lowecase
+    # In questo caso avremmo "school"
+
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
