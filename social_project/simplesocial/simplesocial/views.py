8ff348dddf45e0edf5ae9a1a06a235be8b874aab
Otakar81Cube <34288632+Otakar81@users.noreply.github.com>
2018-02-25 11:58:01 +0100

    Simple social

---
+++ social_project/simplesocial/simplesocial/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+# Home page del sito
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
